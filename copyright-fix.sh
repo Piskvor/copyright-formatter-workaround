3ddb0e0f80bce9e9a48ecb7e389a9845d0d2c087
Jan Piskvor Martinec <github@piskvor.org>
2018-09-20 12:14:07 +0200

    copyright fix now supports process substitution

--- copyright-fix.sh
+++ copyright-fix.sh
@@ -49,6 +49,9 @@ set -e
 
 
 FILE=${1:-}
+if ( echo "$FILE" | grep -c "^/dev/fd" ) ; then
+    FILE="$(cat ${FILE})"
+fi
 if [ "$FILE" = "" ] ; then
 
     RP=$(realpath $0)
@@ -97,25 +100,27 @@ else
         FILE=${2:-"what"}
         grep -Hcr '@copyright' ${FILE} | grep -F '.php' | grep -Ev ':1' | sed 's/^/Problematic copyrights: /' >&2
     else
-        TMPFILE_OLD=$(mktemp)
-        TMPFILE_NEW=$(mktemp)
-        # clean up after we're done (we are expanding the variables now)
-        # shellcheck disable=SC2064
-        trap "rm ${TMPFILE_NEW} ${TMPFILE_OLD} 2>/dev/null || true" INT EXIT
-        # while we could operate on the actual file, in practice that sometimes truncates the file
-        cp "${FILE}" "${TMPFILE_OLD}"
-        OLD_HASH=$(sha1sum < "${FILE}")
-        # we assume that the copyright block is right at line 2 and LF ("UN*X-style") linebreaks
-        tr '\n' '\r' < ${TMPFILE_OLD} | sed 's~<?php.\/\*\*\+~<?php\r/*~' | tr '\r' '\n' > ${TMPFILE_NEW} || true
-        NEW_HASH=$(sha1sum < "${TMPFILE_NEW}")
-        if [ "$OLD_HASH" != "$NEW_HASH" ]; then
-            if [ "$(wc -l < "${TMPFILE_NEW}")" -lt 5 ]; then
-                echo -e  "\e[91mBAD FIX: $FILE\e[0m"
-                exit 4
+        for i in ${FILE} ; do
+            TMPFILE_OLD=$(mktemp)
+            TMPFILE_NEW=$(mktemp)
+            # clean up after we're done (we are expanding the variables now)
+            # shellcheck disable=SC2064
+            trap "rm ${TMPFILE_NEW} ${TMPFILE_OLD} 2>/dev/null || true" INT EXIT
+            # while we could operate on the actual file, in practice that sometimes truncates the file
+            cp "${i}" "${TMPFILE_OLD}"
+            OLD_HASH=$(sha1sum < "${i}")
+            # we assume that the copyright block is right at line 2 and LF ("UN*X-style") linebreaks
+            tr '\n' '\r' < ${TMPFILE_OLD} | sed 's~<?php.\/\*\*\+~<?php\r/*~' | tr '\r' '\n' > ${TMPFILE_NEW} || true
+            NEW_HASH=$(sha1sum < "${TMPFILE_NEW}")
+            if [ "$OLD_HASH" != "$NEW_HASH" ]; then
+                if [ "$(wc -l < "${TMPFILE_NEW}")" -lt 5 ]; then
+                    echo -e  "\e[91mBAD FIX: $i\e[0m"
+                    exit 4
+                fi
+                cp "${TMPFILE_NEW}" "${i}"
+                log-copyright-actions "${i}"
+                post-copyright-fix "${i}"
             fi
-            cp "${TMPFILE_NEW}" "${FILE}"
-            log-copyright-actions "${FILE}"
-            post-copyright-fix "${FILE}"
-        fi
+        done
     fi
 fi
